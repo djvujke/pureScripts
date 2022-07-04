5ad266a49f124394ec662a2982c834a7f65843ed
djvujke <djordje.vujnovic@gmail.com>
2022-07-04 11:43:04 +0200

    Update prepare_env.sh
    replication config changed - wrongly created on 2nd node instead of 1st

--- prepare_env.sh
+++ prepare_env.sh
@@ -31,6 +31,18 @@ $SSH $STRG1 purevol add --pgroup B1POD:B1PG B1Data
 $SSH $STRG1 purevol add --pgroup B1POD:B1PG B1Log
 $SSH $STRG1 purevol add --pgroup B1POD:B1PG B1Path
 
+$SSH $STRG1 purepod create B2POD
+$SSH $STRG1 purepgroup create B2POD::B2PG
+$SSH $STRG1 purevol add --pgroup B2POD:B2PG B2Data
+$SSH $STRG1 purevol add --pgroup B2POD:B2PG B2Log
+$SSH $STRG1 purevol add --pgroup B2POD:B2PG B2Path
+
+$SSH $STRG1 purepod create B3POD
+$SSH $STRG1 purepgroup create B3POD::B3PG
+$SSH $STRG1 purevol add --pgroup B3POD:B3PG B3Data
+$SSH $STRG1 purevol add --pgroup B3POD:B3PG B3Log
+$SSH $STRG1 purevol add --pgroup B3POD:B3PG B3Path
+
 # On STRG2  
 $SSH $STRG2 purepod create B1POD-DR
 $SSH $STRG2 purepod demote B1POD-DR
@@ -47,24 +59,3 @@ $SSH $STRG1 purepod replica-link create B2POD --remote-pod B2POD-DR --remote $ST
 $SSH $STRG1 purepod replica-link create B3POD --remote-pod B3POD-DR --remote $STRG2
 
 $SSH $STRG1 purepod replica-link list
-
-# Create replication between STRG2 and STRG2
-
-$SSH $STRG2 purepod create B2POD
-$SSH $STRG2 purepgroup create B2POD::B2PG
-$SSH $STRG2 purevol add --pgroup B2POD:B2PG B2Data
-$SSH $STRG2 purevol add --pgroup B2POD:B2PG B2Log
-$SSH $STRG2 purevol add --pgroup B2POD:B2PG B2Path
-
-$SSH $STRG2 purepod create B2POD
-$SSH $STRG2 purepgroup create B2POD::B2PG
-$SSH $STRG2 purevol add --pgroup B2POD:B2PG B2Data
-$SSH $STRG2 purevol add --pgroup B2POD:B2PG B2Log
-$SSH $STRG2 purevol add --pgroup B2POD:B2PG B2Path
-
-$SSH $STRG2 purepod create B2POD
-$SSH $STRG2 purepgroup create B2POD::B2PG
-$SSH $STRG2 purevol add --pgroup B2POD:B2PG B2Data
-$SSH $STRG2 purevol add --pgroup B2POD:B2PG B2Log
-$SSH $STRG2 purevol add --pgroup B2POD:B1PG B1Path
-
